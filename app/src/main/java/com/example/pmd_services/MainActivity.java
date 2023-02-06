dee5a5666863e91d814d6b1a6b638ce10270c9da
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-06 08:53:45 +0100

    creacion main

--- app/src/main/java/com/example/pmd_services/MainActivity.java
+++ app/src/main/java/com/example/pmd_services/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.pmd_services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bontonIniciar = (Button) findViewById(R.id.botonIniciar);
+        Button botonDetener = (Button) findViewById(R.id.botonDetener);
+        bontonIniciar.setOnClickListener(MainActivity.this);
+        botonDetener.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.botonIniciar:
+                break;
+            case R.id.botonDetener:
+                break;
+        }
+
     }
 }
